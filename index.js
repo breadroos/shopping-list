ee21f6462eb7582a788917241a4a591af3aa1045
Brett Roos <brettroos60@gmail.com>
2019-10-08 11:54:33 -0700

    updated js file

--- index.js
+++ index.js
@@ -6,6 +6,20 @@
 
 //Hint: you may find it helpful to read up on and use the following jQuery methods: .submit(), preventDefault(), toggleClass(), and closest().
 
+
+$(document).ready(function() {
+  //on the click of the delete button, find the nearest list item, and remove it.
+  $('.shopping-list').on('click', '.shopping-item-delete', function(e) {
+    $(this).closest('li').remove();
+  });
+
+  //on the click of the check button, target the specific event, and then find the closest list item, and find the item that is in that list item, and toggle the css class provided for toggleClass.
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(e){
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+});
+
+//convert this to another function that runs on the page load
 $(function submitItem(){
   $('#js-shopping-list-form').submit(function(event){
     //prevent reload after the submission
@@ -26,15 +40,8 @@ $(function submitItem(){
           </button>
         </div>
       </li>`);
-
-      //on the click of the delete button, find the nearest list item, and remove it.
-      $('.shopping-list').on('click', '.shopping-item-delete', function(e) {
-        $(this).closest('li').remove();
-      })
-
-      //on the click of the check button, target the specific event, and then find the closest list item, and find the item that is in that list item, and toggle the css class provided for toggleClass.
-      $('.shopping-list').on('click', '.shopping-item-toggle', function(e){
-        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-      });
   });
+  //when the window loads, run this function. But I can't have it run when the window is loaded, it needs to be present to be used after the page is loaded
+
+
 });
